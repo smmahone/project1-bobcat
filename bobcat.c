591da3ac12802891dfe327a840ad65bcfe4389a2
smmahone <122935622+smmahone@users.noreply.github.com>
2023-04-09 17:50:27 -0700

    Initial commit

---
+++ bobcat.c
@@ -0,0 +1,9 @@
+#include <err.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
