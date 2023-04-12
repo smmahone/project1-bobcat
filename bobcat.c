fa24289e8486836775b3c33f96e6b33fccee441b
Shauna Mahoney <smmahone@ucsc.edu>
2023-04-12 16:31:57 -0700

    test

--- bobcat.c
+++ bobcat.c
@@ -6,4 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+int main(int argc, char *argv[]) { 
+	printf("test");
+	return EXIT_SUCCESS; 
+}
