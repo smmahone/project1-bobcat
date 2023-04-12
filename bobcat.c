0cb1f9c8806d35f85cf4ab73a88743a48addd434
Shauna Mahoney <smmahone@ucsc.edu>
2023-04-12 16:38:07 -0700

    Try

--- bobcat.c
+++ bobcat.c
@@ -5,8 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-int main(int argc, char *argv[]) { 
-	printf("test");
-	return EXIT_SUCCESS; 
+int main(int argc, char *argv[]) {
+  FILE *fptr;
+  char total[4096];
+  for (int x = 1; x < argc; x++) {
+    fptr = fopen(argv[x], "r");
+    while (fgets(total, 4096, fptr) != NULL) {
+      printf("%s", total);
+    }
+  }
+  fclose(fptr);
+  return EXIT_SUCCESS;
 }
+//I had a horrible time figuring out how to push this, ask the TAs ^^;, luckily I saved my finished project so once I did finally figure
+//it out, I could put it back in.
