733f508086897bc8d75cb7ee1fbd748a467cf878
Shauna Mahoney <smmahone@ucsc.edu>
2023-04-13 14:53:28 -0700

    touch up

--- bobcat.c
+++ bobcat.c
@@ -8,14 +8,26 @@
 int main(int argc, char *argv[]) {
   FILE *fptr;
   char total[4096];
+  int success_check = 0;
   for (int x = 1; x < argc; x++) {
     fptr = fopen(argv[x], "r");
-    while (fgets(total, 4096, fptr) != NULL) {
-      printf("%s", total);
-    }
+    if (fptr == NULL) {
+      fprintf(stderr, "bobcat: %s: No such file or directory\n", argv[x]);
+      success_check++;
+    } else {
+        while (fgets(total, 4096, fptr) != NULL) {
+          printf("%s", total);
+        }
+      }
   }
-  fclose(fptr);
-  return EXIT_SUCCESS;
+  if (fptr != NULL) {
+    fclose(fptr);
+  }
+  if (success_check != 0) {
+    return success_check;
+  } else {
+    return EXIT_SUCCESS;
+  }
 }
 //I had a horrible time figuring out how to push this, ask the TAs ^^;, luckily I saved my finished project so once I did finally figure
 //it out, I could put it back in.
